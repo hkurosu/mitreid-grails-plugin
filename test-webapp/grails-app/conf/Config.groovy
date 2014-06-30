b2788bf488d5dd457f8a76624f4ab121f4133898
Hirobumi Kurosu <hirobumi.kurosu@accselrys.com>
2014-06-30 14:20:01 -0700

    logger options

--- test-webapp/grails-app/conf/Config.groovy
+++ test-webapp/grails-app/conf/Config.groovy
@@ -110,21 +110,22 @@ log4j = {
     //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
     //}
 
-    debug  'org.springframework'
+    debug  'org.springframework',
+            'org.codehaus.groovy.grails.web'
 
-    error  'org.codehaus.groovy.grails.web.servlet',        // controllers
-           'org.codehaus.groovy.grails.web.pages',          // GSP
-           'org.codehaus.groovy.grails.web.sitemesh',       // layouts
-           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping',        // URL mapping
-           'org.codehaus.groovy.grails.commons',            // core / classloading
-           'org.codehaus.groovy.grails.plugins',            // plugins
-           'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
+//    error  'org.codehaus.groovy.grails.web.servlet',        // controllers
+//           'org.codehaus.groovy.grails.web.pages',          // GSP
+//           'org.codehaus.groovy.grails.web.sitemesh',       // layouts
+//           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+//           'org.codehaus.groovy.grails.web.mapping',        // URL mapping
+//           'org.codehaus.groovy.grails.commons',            // core / classloading
+//           'org.codehaus.groovy.grails.plugins',            // plugins
+      error     'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
  //          'org.springframework',
            'org.springframework.env',
            'org.springframework.framework.CglibAopProxy',
-           'org.hibernate',
-           'net.sf.ehcache.hibernate'
+          'org.hibernate',
+          'net.sf.ehcache.hibernate'
 
 
 }
